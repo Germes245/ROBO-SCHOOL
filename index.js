84f0ebcf9c4e7968e7815b93d5f252dff5c9a102
artem <dan119@bk.ru>
2025-09-08 13:23:54 +0300

    swiper in doing

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-//const burger_button=document.querySelector('#burger_button');
-const swiper = new Swiper('.swiper', {// Navigation arrows
-  navigation: {
-    nextEl: '#right_button',
-    prevEl: '#left_button',
-  },
-
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 3,
-  spaceBetween: 40, // Отступ между слайдами
-  
-  scrollbar : {
-    el: '.swiper-scrollbar',
-    draggable: true,
+function act_swiper(){
+  let slidesPerView=3;
+  if(window.innerWidth<1200){
+    slidesPerView=2;
   }
-});
+  if(window.innerWidth<700){
+    slidesPerView=1;
+  }
+  new Swiper('.swiper', {// Navigation arrows
+    navigation: {
+      nextEl: '#right_button',
+      prevEl: '#left_button',
+    },
 
-document.querySelectorAll('.more_details').addEventListener('click', (event)=>{
-  
-});
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: slidesPerView,
+    spaceBetween: 40, // Отступ между слайдами
+    
+    scrollbar : {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    }
+  });
+};
+act_swiper();
+window.addEventListener('resize', act_swiper);
 
 /*burger_button.addEventListener('click',()=>{
   console.log(burger_button.classList);
